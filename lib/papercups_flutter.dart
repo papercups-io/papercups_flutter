8bdd3efb3b5e37601fd2be49561bfe5d868dccdc
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-11-17 19:34:05 +0100

    🚧 Initial Rough Implementation
    Not necessarilly working yet!

--- lib/papercups_flutter.dart
+++ lib/papercups_flutter.dart
@@ -1,7 +1,69 @@
 library papercups_flutter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class CustomerMetadata {
+  String name;
+  String email;
+  String externalId;
+  Map<String, String> otherMetadata;
+
+  CustomerMetadata({
+    this.email,
+    this.externalId,
+    this.name,
+    this.otherMetadata,
+  });
+}
+
+class Props {
+  String title;
+  String subtitle;
+  String primaryColor;
+  String accountId;
+  String baseUrl;
+  String greeting;
+  CustomerMetadata customer;
+  String newMessagePlaceholder;
+  String agentAvailableText;
+  String agentUnavailableText;
+  bool showAgentAvailability;
+  bool requireEmailUpfront;
+  bool scrollEnabled;
+
+  Props({
+    @required this.accountId,
+    this.agentAvailableText = "We're online",
+    this.agentUnavailableText = "We are currently not available",
+    this.baseUrl = "https://app.papercups.io",
+    this.customer,
+    this.greeting = "Welcome!",
+    this.newMessagePlaceholder = "Start typing...",
+    this.primaryColor = "#13c2c2",
+    this.requireEmailUpfront = false,
+    this.scrollEnabled = true,
+    this.showAgentAvailability = true,
+    this.subtitle = "Ask us anything in the chat window below 😊",
+    this.title = "Welcome to Papercups!",
+  });
+}
+
+class PaperCupsWidget extends StatelessWidget {
+  final Props props;
+  final String iframeUrl;
+  PaperCupsWidget({
+    this.iframeUrl = "https://chat-widget.papercups.io",
+    @required this.props,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    print(json.encode(props));
+    return WebView(
+      initialUrl: iframeUrl + json.encode(props),
+    );
+  }
 }
--- test/papercups_flutter_test.dart
+++ test/papercups_flutter_test.dart
@@ -3,11 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:papercups_flutter/papercups_flutter.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  //TODO: Add Unit tests
+  PaperCupsWidget(props: null);
 }
