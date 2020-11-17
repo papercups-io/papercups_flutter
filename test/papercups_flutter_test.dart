f258a63cb2001e34a6b954c6d2b5543f23c5a66d
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-11-17 20:03:00 +0100

    🚨 Fix lint warnings

--- test/papercups_flutter_test.dart
+++ test/papercups_flutter_test.dart
@@ -1,8 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:papercups_flutter/papercups_flutter.dart';
-
 void main() {
   //TODO: Add Unit tests
-  PaperCupsWidget(props: null);
 }
