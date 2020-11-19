4f3ec4543214f24ff710eed1b956f7e1d4f49133
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-11-19 22:21:43 +0100

    🚧 Initial Web support
    Untested but should work

--- lib/papercups_flutter.dart
+++ lib/papercups_flutter.dart
@@ -1,8 +1,12 @@
 library papercups_flutter;
 
 // Imports.
+import 'dart:html' as html;
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -29,20 +33,48 @@ class PaperCupsWidget extends StatefulWidget {
 }
 
 class _PaperCupsWidgetState extends State<PaperCupsWidget> {
+  html.IFrameElement _iframeElement;
+  Widget _iframeWidget;
+
   @override
   void initState() {
     //Enables SurfaceAndroidWebView, much better keyboard support. This is the reason for needing a stateful widget!
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
     super.initState();
+    if (kIsWeb) _iframeElement = html.IFrameElement();
   }
 
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      // Invokes the genIgrameUrl and passes the required paramaters, will return a url, which will be shown on the webview.
-      initialUrl: genIframeUrl(widget.props, widget.iframeUrl, context),
-      // Needs to be unrestricted, default blocks all JS from running.
-      javascriptMode: JavascriptMode.unrestricted,
-    );
+    if (kIsWeb) {
+      return LayoutBuilder(
+        builder: (ctx, constraints) {
+          _iframeElement.height = constraints.maxHeight.toString();
+          _iframeElement.width = constraints.maxWidth.toString();
+          _iframeElement.src =
+              genIframeUrl(widget.props, widget.iframeUrl, context);
+          _iframeElement.style.border = 'none';
+
+          // ignore: undefined_prefixed_name
+          ui.platformViewRegistry.registerViewFactory(
+            'papercupsIFrame',
+            (int viewId) => _iframeElement,
+          );
+
+          _iframeWidget = HtmlElementView(
+            key: UniqueKey(),
+            viewType: 'papercupsIFrame',
+          );
+
+          return _iframeWidget;
+        },
+      );
+    } else
+      return WebView(
+        // Invokes the genIgrameUrl and passes the required paramaters, will return a url, which will be shown on the webview.
+        initialUrl: genIframeUrl(widget.props, widget.iframeUrl, context),
+        // Needs to be unrestricted, default blocks all JS from running.
+        javascriptMode: JavascriptMode.unrestricted,
+      );
   }
 }
