14db6cdb925cba9f4fb001a5d2ffa43b324d225e
Eduardo <eduardomoreno.eduardo@gmail.com>
2020-11-23 12:10:25 +0100

    📝 Add docs for platformViewRegistry

--- lib/platformViewStub.dart
+++ lib/platformViewStub.dart
@@ -1,4 +1,5 @@
 // ignore: camel_case_types
+/// This class is just a stub to make the Flutter analyzer happy about platformViewRegistry.
 class platformViewRegistry {
   static registerViewFactory(String viewId, dynamic cb) {}
 }
