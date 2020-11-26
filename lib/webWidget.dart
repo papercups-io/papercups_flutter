6d54913f819dbe1a5b41aa26e240a47314e96540
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-11-26 18:27:35 +0100

    📝 Add some docs

--- lib/webWidget.dart
+++ lib/webWidget.dart
@@ -20,23 +20,31 @@ class ChatWidget extends StatelessWidget {
     return LayoutBuilder(
       builder: (ctx, constraints) {
         html.IFrameElement _iframeElement;
+
+        // Iframe configuration.
         _iframeElement = html.IFrameElement();
         _iframeElement.height = constraints.maxHeight.toString();
         _iframeElement.width = constraints.maxWidth.toString();
         _iframeElement.src =
             genIframeUrl(widget.props, widget.iframeUrl, context);
         _iframeElement.style.border = 'none';
+
+        // Invocation of funtions is an event is sent through the stream.
         _iframeElement.onLoad.listen((event) {
           widget.onFinishLoading();
         });
         _iframeElement.onError.listen((event) {
           widget.onError(null);
         });
+
         // ignore: undefined_prefixed_name
         ui.platformViewRegistry.registerViewFactory(
           'papercupsIFrame',
           (int viewId) => _iframeElement,
         );
+
+        // We use const here to stop the widget from rebuilding if setState
+        // is called as the widget does not change.
         return const HtmlElementView(
           key: Key("iframepaperCups"),
           viewType: 'papercupsIFrame',
