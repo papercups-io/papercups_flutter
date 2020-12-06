e5ee4c996641bc06240b2645f95eba0b37c70f66
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-12-06 19:53:06 +0100

    🚧 Create conversation model

---
+++ lib/models/conversation.dart
@@ -0,0 +1,21 @@
+class Conversation {
+  String accountId;
+  String asigneeId;
+  String createdAt;
+  String customerId;
+  String id;
+  String priority;
+  bool read;
+  String status;
+
+  Conversation({
+    this.accountId,
+    this.asigneeId,
+    this.createdAt,
+    this.customerId,
+    this.id,
+    this.priority,
+    this.read,
+    this.status,
+  });
+}
