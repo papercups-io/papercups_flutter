54a203fee066d24b6832fe5bd253d7194bcaade5
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-12-08 17:53:09 +0100

    🐛 Fix example Mediaquery being null on non deviceFrame envs

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,9 +27,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.cyan,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      themeMode: MediaQuery.of(context).platformBrightness == Brightness.dark
-          ? ThemeMode.dark
-          : ThemeMode.light,
+      themeMode: MediaQuery.maybeOf(context) == null
+          ? ThemeMode.system
+          : MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? ThemeMode.dark
+              : ThemeMode.light,
       home: MyHomePage(),
     );
   }
