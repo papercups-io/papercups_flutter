7e493eb644c46105525d68ae25906981f3055a24
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-12-19 19:38:18 +0100

    🥅 Catch very broad error here
    Empty data recieved?

--- lib/utils/getPastCustomerMessages.dart
+++ lib/utils/getPastCustomerMessages.dart
@@ -16,51 +16,57 @@ Future<Map<String, dynamic>> getPastCustomerMessages(
     }),
     headers: {"Accept": "*/*"},
   );
-  var data = jsonDecode(res.body)["data"][0];
-  data["messages"].forEach((val) {
-    rMsgs.add(
-      PapercupsMessage(
-        accountId: val["account_id"],
-        body: val["body"],
-        createdAt: DateTime.tryParse(val["created_at"]),
-        sentAt: DateTime.tryParse(val["sent_at"]),
-        conversationId: val["conversation_id"],
-        customerId: val["customer_id"],
-        customer: c,
-        id: val["id"],
-        userId: val["user_id"],
-        user: val["user"] != null
-            ? User(
-                email: val["user"]["email"],
-                id: val["user"]["id"],
-                role: val["user"]["role"],
-                fullName: (val["user"]["full_name"] != null)
-                    ? val["user"]["full_name"]
-                    : null,
-                profilePhotoUrl: (val["user"]["profile_photo_url"] != null)
-                    ? val["user"]["profile_photo_url"]
-                    : null,
-              )
-            : null,
-      ),
+  try {
+    var data = jsonDecode(res.body)["data"][0];
+    data["messages"].forEach((val) {
+      rMsgs.add(
+        PapercupsMessage(
+          accountId: val["account_id"],
+          body: val["body"],
+          createdAt: DateTime.tryParse(val["created_at"]),
+          sentAt: DateTime.tryParse(val["sent_at"]),
+          conversationId: val["conversation_id"],
+          customerId: val["customer_id"],
+          customer: c,
+          id: val["id"],
+          userId: val["user_id"],
+          user: val["user"] != null
+              ? User(
+                  email: val["user"]["email"],
+                  id: val["user"]["id"],
+                  role: val["user"]["role"],
+                  fullName: (val["user"]["full_name"] != null)
+                      ? val["user"]["full_name"]
+                      : null,
+                  profilePhotoUrl: (val["user"]["profile_photo_url"] != null)
+                      ? val["user"]["profile_photo_url"]
+                      : null,
+                )
+              : null,
+        ),
+      );
+    });
+    var customerData = data["customer"];
+    c = PapercupsCustomer(
+      createdAt: DateTime.tryParse(customerData["created_at"]),
+      email: customerData["email"],
+      externalId: customerData["external_id"],
+      firstSeen: DateTime.tryParse(customerData["first_seen"]),
+      id: customerData["id"],
+      lastSeen: DateTime.tryParse(customerData["last_seen"]),
+      updatedAt: DateTime.tryParse(customerData["updated_at"]),
+      name: customerData["name"],
+      phone: customerData["phone"],
     );
-  });
-  var customerData = data["customer"];
 
-  c = PapercupsCustomer(
-    createdAt: DateTime.tryParse(customerData["created_at"]),
-    email: customerData["email"],
-    externalId: customerData["external_id"],
-    firstSeen: DateTime.tryParse(customerData["first_seen"]),
-    id: customerData["id"],
-    lastSeen: DateTime.tryParse(customerData["last_seen"]),
-    updatedAt: DateTime.tryParse(customerData["updated_at"]),
-    name: customerData["name"],
-    phone: customerData["phone"],
-  );
-
-  return {
-    "msgs": rMsgs,
-    "cust": c,
-  };
+    return {
+      "msgs": rMsgs,
+      "cust": c,
+    };
+  } catch (e) {
+    return {
+      "msgs": null,
+      "cust": c,
+    };
+  }
 }
