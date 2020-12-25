9f8e097eac52f46c637376cebc751339716c5a41
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-12-25 12:14:43 +0100

    📝 Add docs to conversation.dart

--- lib/models/conversation.dart
+++ lib/models/conversation.dart
@@ -1,11 +1,28 @@
+/// This is the class which contains the details of the conversation.
 class Conversation {
+  /// The account ID used for the conversation.
   String accountId;
+
+  /// Who has the conversation assigned on the papercups dashboard.
   String asigneeId;
+
+  /// When the conversation was created.
   String createdAt;
+
+  /// The customer ID, should be unique to the person,
+  /// may be assigned to multiple conversations.
   String customerId;
+
+  /// Unique ID used to identify the conversation.
   String id;
+
+  /// How much priority the ocnvesation has.
   String priority;
+
+  /// If the convesation has been read by an agent.
   bool read;
+
+  /// The status of a conversation, can be open or closed.
   String status;
 
   Conversation({
