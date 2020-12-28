e1f86a8b2f2f480955184f9e138ec81258b546eb
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-12-28 15:20:05 +0100

    🥅 Catch errors on getCustomerDetailsFromMetadata.dart

--- lib/utils/getCustomerDetailsFromMetadata.dart
+++ lib/utils/getCustomerDetailsFromMetadata.dart
@@ -12,28 +12,32 @@ Future<PapercupsCustomer> getCustomerDetailsFromMetadata(
   if (client == null) {
     client = Client();
   }
-  var res = await client.get(
-    Uri.https(
-      p.baseUrl,
-      "/api/customers/identify",
-      {
-        "external_id": p.customer.externalId,
-        "account_id": p.accountId,
-      },
-    ),
-  );
-  var data = jsonDecode(res.body)["data"];
-  c = PapercupsCustomer(
-    id: data["customer_id"],
-    externalId: c == null ? null : c.externalId,
-    email: c == null ? null : c.email,
-    createdAt: c == null ? null : c.createdAt,
-    firstSeen: c == null ? null : c.firstSeen,
-    lastSeen: c == null ? null : c.lastSeen,
-    name: c == null ? null : c.name,
-    phone: c == null ? null : c.phone,
-    updatedAt: c == null ? null : c.updatedAt,
-  );
+  try {
+    var res = await client.get(
+      Uri.https(
+        p.baseUrl,
+        "/api/customers/identify",
+        {
+          "external_id": p.customer.externalId,
+          "account_id": p.accountId,
+        },
+      ),
+    );
+    var data = jsonDecode(res.body)["data"];
+    c = PapercupsCustomer(
+      id: data["customer_id"],
+      externalId: c == null ? null : c.externalId,
+      email: c == null ? null : c.email,
+      createdAt: c == null ? null : c.createdAt,
+      firstSeen: c == null ? null : c.firstSeen,
+      lastSeen: c == null ? null : c.lastSeen,
+      name: c == null ? null : c.name,
+      phone: c == null ? null : c.phone,
+      updatedAt: c == null ? null : c.updatedAt,
+    );
+  } catch (e) {
+    c = null;
+  }
   sc(c);
   client.close();
   return c;
