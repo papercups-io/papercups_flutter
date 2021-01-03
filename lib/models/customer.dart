0e59e72626517b0446d8f03242971346d66d3885
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-01-03 12:36:12 +0100

    📝 Add PapercupsCustomer documentation

--- lib/models/customer.dart
+++ lib/models/customer.dart
@@ -1,12 +1,30 @@
+/// This is teh class which contains all customer info.
 class PapercupsCustomer {
+  /// Date of Customer creation.
   DateTime createdAt;
+
+  /// Email of the customer.
   String email;
+
+  /// Name of the customer.
   String name;
+
+  /// Phone of the customer
   String phone;
+
+  /// External ID used to identify the customer with the Papercups API.
   String externalId;
+
+  /// The time where the customer was first seen.
   DateTime firstSeen;
+
+  /// The unique identifier of the customer on the Papercups system.
   String id;
+
+  /// When the customer was last seen.
   DateTime lastSeen;
+
+  /// When the customer details were last updated.
   DateTime updatedAt;
 
   PapercupsCustomer({
