44c15bdfedd0d2f5d618ed0321f9225eade3af4e
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-02-13 19:17:35 +0100

    Update example
    WIP, similar to https://app.papercups.io/demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:papercups_flutter/papercups_flutter.dart';
 
@@ -31,6 +32,29 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var show = true;
+
+  TextEditingController titleController = TextEditingController(text: "Welcome to Papercups!");
+  TextEditingController subtitleController = TextEditingController(text: "Ask us anything using the chat window 💭");
+  Color color = Color(0xff1890ff);
+
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    titleController.addListener(() { 
+      setState(() {});
+      });
+    subtitleController.addListener(() { 
+      setState(() {});
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,9 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         height: 55,
         width: 55,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.blue, Colors.lightBlueAccent],
-          ),
+          color: color,
           shape: BoxShape.circle,
         ),
         child: InkWell(
@@ -58,16 +80,84 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Stack(
-        alignment: Alignment.center,
+        alignment: Alignment.topLeft,
         children: [
-          Text(
-            "Open the chat widget!",
-            style: Theme.of(context).textTheme.headline4,
+          Container(
+            padding: EdgeInsets.only(left: 50, top: 50, right: 50),
+            constraints: BoxConstraints(
+              maxWidth: 800
+            ),
+            child: SingleChildScrollView(
+                          child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Demo", style: Theme.of(context).textTheme.headline3,),
+                  SizedBox(height: 20,),
+                  Text("Hello! Try customizing the chat widget's display text and colors."),
+                  SizedBox(height: 40,),
+                  Text("Update the title:"),
+                  SizedBox(height: 15,),
+                  TextField(
+                    controller: titleController,
+                      decoration: InputDecoration(
+                      hintText: 'Enter a title',
+                      contentPadding: EdgeInsets.all(20.0),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.zero
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xff1890ff)
+                        ),
+                        borderRadius: BorderRadius.zero
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40,),
+                  Text("Update the subtitle:"),
+                  SizedBox(height: 15,),
+                  TextField(
+                    controller: subtitleController,
+                      decoration: InputDecoration(
+                      hintText: 'Enter a subtitle',
+                      contentPadding: EdgeInsets.all(20.0),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.zero
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xff1890ff)
+                        ),
+                        borderRadius: BorderRadius.zero
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40,),
+                  Text("Try changing the color (you can enter any hex value you want!)"),
+                  SizedBox(height: 15,),
+                  ColorPicker(
+                    color: color,
+                    showColorCode: true,
+
+                    pickersEnabled: {
+                      ColorPickerType.accent: false,
+                      ColorPickerType.primary: false,
+                      ColorPickerType.wheel: true,
+                    },
+                    onColorChanged: (c){
+                    setState(() {
+                      color = c;
+                    });
+                  }),
+                ],
+              ),
+            ),
           ),
           Container(
             width: double.infinity,
             alignment: Alignment.bottomRight,
-            margin: const EdgeInsets.only(bottom: 70, top: 50),
+            margin: const EdgeInsets.only(bottom: 70, top: 10),
             child: Visibility(
               visible: show,
               child: Container(
@@ -82,7 +172,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 constraints: BoxConstraints(
                   minWidth: 100,
-                  maxWidth: 400,
+                  maxWidth: 380,
                   minHeight: 100,
                   maxHeight: 800,
                 ),
@@ -97,13 +187,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     },
                     props: Props(
                       accountId: "843d8a14-8cbc-43c7-9dc9-3445d427ac4e",
-                      title: "Welcome!",
-                      //primaryColor: Color(0xff1890ff),
-                      primaryGradient: LinearGradient(
-                        colors: [Colors.blue, Colors.lightBlueAccent],
-                      ),
+                      title: titleController.text,
+                      primaryColor: color,
                       greeting: "Welcome to the test app!",
-                      subtitle: "How can we help you?",
+                      subtitle: subtitleController.text,
                       customer:  null,
                     ),
                   ),
