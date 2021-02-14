1f8f6cfe2d6697277263da12c1eced5a5cdbe513
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-02-14 12:25:39 +0100

    Add docs to parseDateFromUTC.dart

--- lib/utils/parseDateFromUTC.dart
+++ lib/utils/parseDateFromUTC.dart
@@ -1,3 +1,6 @@
+/// This function converts a sting in ISO8601 format into a DateTime object which is in UTC
+/// and then converts in into the local timezone. Used to convert times from the Papercups API.
+/// It assumes all times provided are in UTC or it will force them bo be.
 DateTime parseDateFromUTC(String time) {
   DateTime processed;
 
