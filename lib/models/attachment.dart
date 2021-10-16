05e3c274caf2b8371703d26591603d0e2c4a578e
Aguilaair <eduardomoreno.eduardo@gmail.com>
2021-10-17 01:20:00 +0200

    Revert "Add copyWith to some classes"
    This reverts commit 50982c35425866ec8ed7be8c25f772d0da969d04.

--- lib/models/attachment.dart
+++ lib/models/attachment.dart
@@ -9,20 +9,4 @@ class PapercupsAttachment {
 
   PapercupsAttachment(
       {this.id, this.fileName, this.fileUrl, this.contentType, this.file});
-
-  PapercupsAttachment copyWith({
-    String? id,
-    String? fileName,
-    String? fileUrl,
-    String? contentType,
-    File? file,
-  }) {
-    return PapercupsAttachment(
-      id: id ?? this.id,
-      fileName: fileName ?? this.fileName,
-      fileUrl: fileUrl ?? this.fileUrl,
-      contentType: contentType ?? this.contentType,
-      file: file ?? this.file,
-    );
-  }
 }
